8b7b20fa8f638ea3b9b1fe786bdff6a67efd213b
arda kilic <adk_05@outlook.com>
2024-02-11 02:20:47 +0100

    Added dynamic autocomplete function to search bar

---
+++ autocomplete.js
@@ -0,0 +1,49 @@
+import { getAnimeData } from './fetchData.js';
+import { displayAnimePage } from './pageCreator.js'
+
+
+
+// Create dynamic results box
+
+function createResultsBox() {
+    results.forEach(result => {
+        const resultsContainer = document.createElement('div');
+        resultsContainer.classList.add('result-box')
+    
+        const resultsUl = document.createElement('ul');
+        const resultsListEl = document.createElement('li');
+        const resultsLink = document.createElement('a');
+        resultsLink.setAttribute('a', displayAnimePage(result.title));
+
+        resultsListEl.appendChild(resultsLink);
+        resultsUl.appendChild(resultsListEl);
+        resultsContainer.appendChild(resultsUl);
+    });
+}
+
+// Update results box
+function updateResults(results) {
+    const resultsContainer = document.querySelector('.result-box');
+
+    // Clear previous results
+    resultsContainer.innerHTML = ''; 
+
+    results.forEach(result => {
+        const resultsListEl = document.createElement('li');
+        const resultsLink = document.createElement('a');
+        resultsLink.textContent = result.title;
+        resultsLink.setAttribute('href', '#');
+        resultsLink.addEventListener('click', () => displayAnimePage(result.title));
+
+        resultsListEl.appendChild(resultsLink);
+        resultsContainer.appendChild(resultsListEl);
+    });
+}
+
+
+async function displayResults() {
+    const results = await getAnimeData();
+    updateResults(results)
+}
+
+export { createResultsBox, displayResults, updateResults};
