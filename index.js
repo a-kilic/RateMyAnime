af8ef0cb1cabcb0475ccf383bd0161d0b92f53b0
arda kilic <adk_05@outlook.com>
2024-02-11 02:42:09 +0100

    Added event listener for searchInput

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import { displayAnimePage } from './pageCreator.js';
+import { displayResults } from './autocomplete.js';
+import { searchInput } from './fetchData.js'; 
 
 // Variables
 const formEl = document.getElementById('form');
 
 // Event Listeners
 formEl.addEventListener('submit', async function(e) {
-    e.preventDefault();
+    e.preventDefault(); 
     await displayAnimePage();
 });
 
+searchInput.addEventListener('input', async () => {
+    await displayResults();
+})
